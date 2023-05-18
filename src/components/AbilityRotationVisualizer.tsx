efd6452ebf132db4b12714fa94077ce9bb36881c
Victor Miclotte <victor.miclotte@gmail.com>
2023-05-18 18:40:32 +0200

    WIP2

--- src/components/AbilityRotationVisualizer.tsx
+++ src/components/AbilityRotationVisualizer.tsx
@@ -9,6 +9,9 @@ import {
   captureHoldFullRs,
   mixColor,
 } from '@alt1/base/dist'
+import { findReadLine } from '@alt1/ocr'
+
+import { font } from './font'
 
 const findNextIndex = (abilities: Ability[], currentIndex: number): number => {
   let nextIndex = currentIndex + 1
@@ -22,7 +25,7 @@ const findNextIndex = (abilities: Ability[], currentIndex: number): number => {
 }
 
 const base64PhaseImage =
-  'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAC30lEQVRYR+2UX0hTcRTHv3PTa1HmKtGk4YsWlSnON1suayqZ9ZCbisR8sVVIJkQMhJ6CsL9mVlJaIMUm/smIwIdAwXRao0CICMUHUYjSzW3NP1PvbpzfuNeb2Fvgyz0v93d+5/zOOfdzzu+nSk1OEAQuFqrQEgS1Bip+FaKQHl4OISqGY/ukk6z3EW3i/kZx1vuIOVRUQE/ve6RnZEqJXR8GUFluZjrZai5WYWpyUrLLFxsVJf+BjQqW77EC+lxuFBTk4/vYODtbX1+PaLUazQ23IdoW/R62979FKsCUn49QIJJEl5KCF44O6A+nY/TrN3xyu1FijhDpffcWNReq2Jr82pxd7Evy7HET7ty8wdZElOiJQmfoLImt+jKu1V2PtFMkIC+AjLwQxpXaq5iYmMCMxwOjIQfBxRDTjYYjiFXxLMCevTqMfB7FCs/D6/UiOyMd6vAK2tq78KixAc7uN8xPq9WCAw+LxYLyc1acKS6CJzC/VoD4F+RMM1BcdJIdJAI2mw39/X3QJWhZS6jY1d9ziIpSsWCFp07DkGtk/mlpqQgH/TAYjWhsbsGTpka8bnfA7/chHBbwsqMbOUdzJTJ/ERgbj8wAyVYuBsm7dkgJg95fbE8sgNpVXVPLUDfcu4uh4Y8YGHKhtLQU/LyPtXIhtAzr+Uuw2+2sbcODA3j4tBUOZzteOZz/boF80OQJKahcd3T24HlrCzo6O5F56CAG3V9wYP8+0MAaj+UxkiS5pkJUWCthLTPDZDKh1l6HohN52LaFi7SAhqXEbJGGUF7Aeptcz8rSS4NG1zQufgf0+mxGgOiIg0bxCo8bMegaYVTPllXg1v0HawRoNTXrQ5J2u3TV2P3WaNgDRbbExN2IoceIi8X09A8k7Yxjvku8gOmfsywY6bwgQJcQD00MB58/wAZNFJohLlrDHrRAcAEzc36o5H+7GWulAIWAQkAhoBBQCCgEFAIKAYXAphP4Az52Y2lqTMyvAAAAAElFTkSuQmCC'
+  'iVBORw0KGgoAAAANSUhEUgAAACoAAAAUCAYAAAD7s6+GAAAFDUlEQVRIS71Xb0xVZRj/He653KsiSoCo686RUlqkEz+0FLlRIJOsDwnoXMO1Kf1xEVtrbG59amv0l8jKldbmcsAAozk3P7TpRogW6w/VWtPxwcnWSkEuXuBezj33tOc55zn3Pccb61Pvxu77Pn9/z+953/O+aPdtKrOg63BHIsFTLZWCRfJw2FY5crZNpTJ6cXTkos8EhB2D/MVGXZOh5PflYDn9JRLQ1pdGLAJEwDyDgOblQSNnmofDrg0XQMUkErbc+VWLYAOKqet2wUqBEo9kHF8ZHC8ez+ST3C5Qf1LHmQvwFSHgNDMFK6BzUAEu7GjJBKxQ2Gaf7GgO2KCdIu/yl1xOcWoR2vrIGksN5AHlBE3PzSJn6TI3KScMKNtF1+G3Eb3EJuCmabpx3HY7JJAd6WkEAgGXY/Hh1rvgnIRkSAwwKwEd6YWkxzmbjuyEIWbMYVsyypp+Pa1W8rCctkPS3g5qbm3D2mJLTSyGqkwNLs45uaG7gAkAFbT4imwxG+8hgUsWF06MCnPCBP0Onv8G5Zu3uL4j3w7h4P4GXpOu9YVDuHH9uj+2y4Qay8Ogs2X8xav22QplRrNluzAyil27avHH1Wus7ujoQDAQwPHOtyG6+dgky/6PsSjQmtpaJGdsMJF16/BFdx8qHi7H2G+/4/vRUextsBk+f+4sWp8/xHOyO9UzwL80Pvv4GN558w2eU4eoGzLIh3xptBx5Ga8dfZ3navceeXQHTg8M4j8DpSCmlcYrba9ifHwcNycnEa3cjvh8ktfRyh0Ia/apXXNvBFd+GINhmpiamsK2zeUIpA2c6h3AR12d6DnzNdsVFBQgBBONjY3Y/2wznt5Tj8mZWbS3t2NVURF3j8Z80lgcqLAiVe6p382OxGhLSwsuXryASHEBbwViP3XnNnJyNE5a9+RTqKyKsn1Z2Qak4zFURqPoOn4Cnxzrwle93YjFppFOW/iy7wy276zy7KCxn39C9c5KFOYvYzl/R9VPglhL8qvX7D1KY2koF2sLV7jA4lN/s0xsaZscaW3jFne+9y4uXf4OQ5dG0NTUBHN2mrfQXHIBzYdfZNao9ZeHh/DhpyfR3dOL0909HrChoM5EMFD186Raqcn9B8avU9fd/YP4/OQJ9PX3Y8tDD2J49EdseuB+0MGLPlbN+49GVU0dDjQfRPO+BtTU1KCt/Sjqn6hG3pKQu7/pq+LuUbmZPKU4n6C9DY3uYVL1dCBUnbreurXCPTCUKH/lClRUbGNGiW05MBSv7vEohkeucJee2XcAb73/Aachv5cOP4dffxnjLnj2aLZv141b01hdsNz9BPG16NwcpCspKUKuc49PTPyJ1ffks23CtDDx1y1OSmvTshApXgk9N4Tp2AwfGBnUWmoxxZ6Jz+Hm7Zjtp+soWVWIMNJ26zeWRizDuW+DLNSQMk3ooTBYrusIpgwYepDverIxkJN5mik2FNCAhiCsjL2u23GUQSAMApMyAD3Icx5ipzw7Jbe2sazUvuvlrUjJGJB9D1BiqZCD8/tw3g7M4N00tr0ezNiIPryEQSwozzqO4wB1fxmsTUqQ4+swEgmOlwHqKPxPOg8VspD3ZfyOLclbDtBcgNFaGFJJkAezA8iNTX7+h7cQQjpuvbzwlbegyzBZ0MOWEkhCeeiKzF8J2Qkg1U+1kxaL3tfRTAHOU5LetAzUn1TdKyowYUn2kCT4t4Lk3xi/nepPvn4isqz/AQgLFYzz8wP3AAAAAElFTkSuQmCC'
 
 type AbilityRotationVisualizerProps = {
   abilityRotation: Ability[]
@@ -65,28 +68,55 @@ export const AbilityRotationVisualizer = ({
   useEffect(() => {
     setInterval(() => {
       if (phaseImgPos) {
-        const phase = captureHold(phaseImgPos.x - 5, phaseImgPos.y - 5, 60, 24)
+        const phase = captureHold(phaseImgPos.x, phaseImgPos.y, 60, 20)
+        const imgFullRs = captureHoldFullRs()
         alt1.overLayRect(
           mixColor(255, 255, 255),
-          phaseImgPos.x - 5,
-          phaseImgPos.y - 5,
-          60,
-          24,
+          phaseImgPos.x,
+          phaseImgPos.y,
+          42,
+          20,
           600,
-          5
+          2
         )
         alt1.overLayRect(
           mixColor(255, 0, 0),
           phaseImgPos.x,
           phaseImgPos.y,
           60,
-          24,
+          20,
           600,
-          5
+          2
         )
-        const text = alt1.bindReadString(phase.handle, 'chat', 10, 12)
+        console.log('phase base64', phase.toData().toPngBase64())
+        const {
+          debugArea,
+          text: myText,
+          fragments,
+        } = findReadLine(
+          imgFullRs.toData(),
+          font,
+          [[255, 255, 255]],
+          phaseImgPos.x,
+          phaseImgPos.y,
+          60,
+          20
+        )
+        console.log(myText)
+        console.log({ debugArea, myText, fragments })
+        alt1.overLayRect(
+          mixColor(0, 255, 0),
+          debugArea.x,
+          debugArea.y,
+          debugArea.w,
+          debugArea.h,
+          600,
+          2
+        )
+        const text = alt1.bindReadString(phase.handle, 'chat', 0, 0)
         const newPhase = text.match(/Phase: (\d{1})/)
         if (newPhase && newPhase.length > 0) {
+          console.log('set phase', newPhase[1])
           setCurrentPhase(parseInt(newPhase[1]))
         }
       }
