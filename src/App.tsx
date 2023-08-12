ab9ee0e4e024ddfc6a1d2e58a1fcfe544bf2efb6
Victor Miclotte <victor.miclotte@gmail.com>
2023-08-12 16:06:08 +0200

    Visualize loaded rotation with ticks

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,35 @@ import './app.css'
 import { Settings } from './components/Settings'
 
 const defaultRotation: Ability[] = [
-  { name: 'commandVengefulGhost', tick: 0, keybind: 'E' },
-  { name: 'commandSkeletonWarrior', tick: 0, keybind: 'S' },
-  { name: 'deathSkulls', tick: 3, keybind: '5' },
-  { name: 'touchOfDeath', tick: 6, keybind: '2' },
-  { name: 'fingerOfDeath', tick: 9, keybind: 'Q' },
-  { name: 'weaponSpecialAttack', tick: 12, keybind: 'O' },
-  { name: 'fingerOfDeath', tick: 15, keybind: 'Q' },
+  { name: 'smokeCloud', tick: 0, keybind: '\\' },
+  { name: 'deathSkulls', tick: 0, keybind: 'd' },
+  { name: 'soulSap', tick: 3, keybind: 'r' },
+  { name: 'touchOfDeath', tick: 6, keybind: 'w' },
+  { name: 'necromancy', tick: 9, keybind: 'q' },
+  { name: 'soulSap', tick: 12, keybind: 'r' },
+  { name: 'livingDeath', tick: 15, keybind: 'f' },
+  { name: 'touchOfDeath', tick: 18, keybind: 'w' },
+  { name: 'deathSkulls', tick: 21, keybind: 'd' },
+  { name: 'fingerOfDeath', tick: 24, keybind: 'e' },
+  { name: 'necromancy', tick: 27, keybind: 'q' },
+  { name: 'necromancy', tick: 30, keybind: 'q' },
+  { name: 'necromancy', tick: 33, keybind: 'q' },
+  { name: 'fingerOfDeath', tick: 36, keybind: 'e' },
+  { name: 'necromancy', tick: 39, keybind: 'q' },
+  { name: 'deathSkulls', tick: 42, keybind: 'd' },
+  { name: 'touchOfDeath', tick: 45, keybind: 'w' },
+  { name: 'necromancy', tick: 48, keybind: 'q' },
+  { name: 'necromancy', tick: 51, keybind: 'q' },
+  { name: 'fingerOfDeath', tick: 54, keybind: 'e' },
+  { name: 'fingerOfDeath', tick: 57, keybind: 'e' },
+  { name: 'necromancy', tick: 60, keybind: 'q' },
+  { name: 'lifeTransfer', tick: 63, keybind: '3' },
+  { name: 'soulSap', tick: 66, keybind: 'r' },
+  { name: 'deathSkulls', tick: 69, keybind: 'd' },
+  { name: 'reflect', tick: 72, keybind: '6' },
+  { name: 'volleyOfSouls', tick: 75, keybind: 'a' },
+  { name: 'touchOfDeath', tick: 78, keybind: 'w' },
+  { name: 'weaponSpecialAttack', tick: 81, keybind: 'F4' },
 ]
 
 function App() {
@@ -43,7 +65,10 @@ function App() {
           <Route
             path="/loader"
             element={
-              <AbilityRotationLoader setAbilityRotation={setAbilityRotation} />
+              <AbilityRotationLoader
+                setAbilityRotation={setAbilityRotation}
+                abilityRotation={abilityRotation}
+              />
             }
           />
           <Route path="/settings" element={<Settings />} />
--- src/components/AbilityRotationLoader.tsx
+++ src/components/AbilityRotationLoader.tsx
@@ -1,22 +1,35 @@
 import './abilityRotationLoader.css'
 import { Dropdown } from './Dropdown'
-import { Ability } from '../abilities'
+import { Ability, abilitiesMap } from '../abilities'
 
 type AbilityRotationLoaderProps = {
   setAbilityRotation: (newValue: Ability[]) => void
+  abilityRotation: Ability[]
 }
 
 export const AbilityRotationLoader = ({
   setAbilityRotation,
+  abilityRotation,
 }: AbilityRotationLoaderProps) => {
   function handleLoad(newValue: Ability[]) {
     setAbilityRotation(newValue)
   }
 
   return (
-    <div className="loader">
-      <label>Load rotation</label>
-      <Dropdown setAbilityRotation={handleLoad} />
+    <div className="loader-container">
+      <div className="loader-container__visualizer">
+        {abilityRotation.map(({ name, tick, keybind }, idx) => (
+          <div key={idx}>
+            <img src={abilitiesMap[name]} width={50} height={50} />
+            <div>{tick}</div>
+            <div>{keybind}</div>
+          </div>
+        ))}
+      </div>
+      <div className="loader-container__loader">
+        <label>Load rotation</label>
+        <Dropdown setAbilityRotation={handleLoad} />
+      </div>
     </div>
   )
 }
